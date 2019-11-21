d06be5c51ee10bd46af78267219f155b5647267d
tintuntint <tintun02@yahoo.com.au>
2019-11-22 10:18:33 +1100

    fix again

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t tintuntint/multi-client:latest -t tintuntint/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t tintuntint/multi-server:latest -t tintuntint/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t tintuntint/multi-worker:latest -t tintuntint/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push tintuntint/multi-client:latest
+docker push tintuntint/multi-server:latest
+docker push tintuntint/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push tintuntint/multi-client:$SHA
+docker push tintuntint/multi-server:$SHA
+docker push tintuntint/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=tintuntint/multi-server:$SHA
+kubectl set image deployments/client-deployment client=tintuntint/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=tintuntint/multi-worker:$SHA
